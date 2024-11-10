4411a1ca34428fed2528b2198fa46eac192d217f
furkan eriç <furkaneric9@gmail.com>
2024-11-10 22:47:26 +0300

    first commit

---
+++ UserIdentity/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.UI.WebControls;
+
+namespace UserIdentity.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+       
+
+    }
+}
